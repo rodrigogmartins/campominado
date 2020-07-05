4fe02ccebd839404f263f2a965cdc7176a9b7e34
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2020-07-04 22:10:03 -0300

    Adicionando styled-components ao componente Flag

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,73 +1,91 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  margin-top: 2px;
+`;
+
+const FlagPole = styled.View`
+  position: absolute;
+  height: 14px;
+  width: 2px;
+  background-color: #222;
+  margin-left: 9px;
+
+  ${({ bigger }) =>
+    bigger
+      ? `
+    height: 28px;
+    width: 4px;
+    margin-left: 16px;
+  `
+      : ''};
+`;
+
+const FlagCloth = styled.View`
+  position: absolute;
+  height: 5px
+  width: 6px;
+  background-color: #F22;
+  marginLeft: 3px;
+
+  ${({ bigger }) =>
+    bigger
+      ? `
+    height: 10px;
+    width: 14px;
+    margin-left: 3px;
+  `
+      : ''}
+`;
+
+const Base1 = styled.View`
+  position: absolute;
+  height: 2px;
+  width: 6px;
+  background-color: #222;
+  margin-left: 7px;
+  margin-top: 10px;
+
+  ${({ bigger }) =>
+    bigger
+      ? `
+    height: 4px
+    width: 12px;
+    margin-left: 12px;
+    margin-top: 20px;  
+  `
+      : ''}
+`;
+
+const Base2 = styled.View`
+  position: absolute;
+  height: 2px;
+  width: 10px;
+  background-color: #222;
+  margin-left: 5px;
+  margin-top: 12px;
+
+  ${({ bigger }) =>
+    bigger
+      ? `
+    height: 4px
+    width: 20px;
+    margin-left: 8px;
+    margin-top: 24px;  
+  `
+      : ''}
+`;
 
 function Flag({ bigger }) {
   return (
-    <View style={styles.container}>
-      <View style={[styles.flagpole, bigger ? styles.flagpoleBigger : null]} />
-      <View style={[styles.flag, bigger ? styles.flagBigger : null]} />
-      <View style={[styles.base1, bigger ? styles.base1Bigger : null]} />
-      <View style={[styles.base2, bigger ? styles.base2Bigger : null]} />
-    </View>
+    <Container>
+      <FlagPole bigger={bigger} />
+      <FlagCloth bigger={bigger} />
+      <Base1 bigger={bigger} />
+      <Base2 bigger={bigger} />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 2,
-  },
-  flagpole: {
-    position: 'absolute',
-    height: 14,
-    width: 2,
-    backgroundColor: '#222',
-    marginLeft: 9,
-  },
-  flag: {
-    position: 'absolute',
-    height: 5,
-    width: 6,
-    backgroundColor: '#F22',
-    marginLeft: 3,
-  },
-  base1: {
-    position: 'absolute',
-    height: 2,
-    width: 6,
-    backgroundColor: '#222',
-    marginLeft: 7,
-    marginTop: 10,
-  },
-  base2: {
-    position: 'absolute',
-    height: 2,
-    width: 10,
-    backgroundColor: '#222',
-    marginLeft: 5,
-    marginTop: 12,
-  },
-  flagpoleBigger: {
-    height: 28,
-    width: 4,
-    marginLeft: 16,
-  },
-  flagBigger: {
-    height: 10,
-    width: 14,
-    marginLeft: 3,
-  },
-  base1Bigger: {
-    height: 4,
-    width: 12,
-    marginLeft: 12,
-    marginTop: 20,
-  },
-  base2Bigger: {
-    height: 4,
-    width: 20,
-    marginLeft: 8,
-    marginTop: 24,
-  },
-});
 
 export default Flag;
